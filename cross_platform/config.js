48af50cccb0c5c196f024247a1f8c6be5d00e825
praguna.manvi <praguna.manvi@dailyhunt.in>
2020-06-17 23:16:45 +0530

    added image optimization and fixed the api end point

--- cross_platform/config.js
+++ cross_platform/config.js
@@ -2,5 +2,5 @@ export const env = {
     server:"http://172.20.10.6:5000/" // vishnu
     // server : "http://192.168.42.2:5000/"  // praguna
     // server : "http://192.168.43.117:5000/"  // nitish
-    // server : "http://18.222.233.117:5000/" // server
+    // server : http://fyp-sifu-env-37.eba-pbfvnxtm.us-east-2.elasticbeanstalk.com/ //server
 }
